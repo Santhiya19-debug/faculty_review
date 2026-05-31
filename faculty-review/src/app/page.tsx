45187a34851eed3a853af04446e6c18536e97226
Santhiya <santhiya19@gmail.com>
2026-05-31 23:20:12 +0530

    blo section added

--- faculty-review/src/app/page.tsx
+++ faculty-review/src/app/page.tsx
@@ -3,6 +3,7 @@ import MainLayout from "@/components/layout/MainLayout";
 import HeroSection from "@/components/shared/HeroSection";
 import FeaturesRow from "@/components/shared/FeaturesRow";
 import TopFaculties from "@/components/shared/TopFaculties";
+import CommunitySnapshot from "@/components/shared/CommunitySnapshot";
 import RecentReviews from "@/components/shared/RecentReviews";
 import RequestCTA from "@/components/shared/RequestCTA";
 
@@ -30,12 +31,16 @@ export default async function HomePage() {
     .order("name");
 
   return (
-    <MainLayout>
-      <HeroSection schools={schools || []} />
-      <FeaturesRow />
-      <TopFaculties faculties={topFaculties || []} />
-      <RecentReviews reviews={recentReviews || []} />
-      <RequestCTA />
-    </MainLayout>
-  );
+  <MainLayout>
+    <HeroSection schools={schools || []} />
+    <FeaturesRow />
+    <TopFaculties faculties={topFaculties || []} />
+
+    <CommunitySnapshot />
+
+    <RequestCTA />
+
+    <RecentReviews reviews={recentReviews || []} />
+  </MainLayout>
+);
 }
